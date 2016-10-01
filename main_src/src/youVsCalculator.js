da35fbdee418bf81301839acdb2454a2d72acf34
Miles Smith <miles71397@gmail.com>
2016-10-01 15:51:43 -0700

    Updated js calculator file

--- main_src/src/youVsCalculator.js
+++ main_src/src/youVsCalculator.js
@@ -4,9 +4,10 @@ function youVsZuckerberg(userIncome) {
   var zuckIncomeDaily = 49315068;
   var zuckIncomeHourly = 2054794;
   var zuckIncomeInMinutes = 34246;
+  var zuckIncomeInSeconds = 570;
 
   if(userIncome >= zuckIncomeYearly) {
-    return console.log("You must be a very important person!");
+    return console.log("You must be apart of the Rothschild's family, or be a king");
   } else if(userIncome >= zuckIncomeMonthly) {
     var monthlyIncomeAnswer = userIncome/zuckIncomeMonthly;
      return console.log("it would take Zuckerberg " +
@@ -19,10 +20,16 @@ function youVsZuckerberg(userIncome) {
     var hourlyIncomeAnswer = userIncome/zuckIncomeHourly;
     return console.log("it would take Zuckerberg " +
     Math.round(hourlyIncomeAnswer * 10)/10 + " hours to earn your income");
-  } else {
+  } else if(userIncome >= zuckIncomeInMinutes){
     var incomeInMinutesAnswer = userIncome/zuckIncomeInMinutes;
     return console.log("it would take Zuckerberg " +
     Math.round(incomeInMinutesAnswer * 10)/10 + " minutes to earn your income");
+  } else if(userIncome >= zuckIncomeInSeconds ) {
+    var incomeInSecondsAnswer = userIncome/zuckIncomeInSeconds;
+    return console.log("It would take Zuckerberg " +
+    Math.round(incomeInSecondsAnswer * 10)/10 + " seconds to earn your income");
+  } else {
+    return console.log("Zuckerberg earned your income so fast its not even calculable")
   }
 }
 
@@ -31,18 +38,22 @@ youVsZuckerberg(1800000000);
 youVsZuckerberg(100000000);
 youVsZuckerberg(9000000);
 youVsZuckerberg(105000);
+youVsZuckerberg(80000);
+youVsZuckerberg(30000);
+youVsZuckerberg(23000);
+youVsZuckerberg(5)
 
 //19285
 function perspective (userIncome) {
   if(userIncome >= zuckIncomeYearly) {
     console.log("We have nothing to put in perspective for you, you win");
   } else if(userIncome >= zuckIncomeMonthly) {
-
+    console.log("We still have nothing to put in perspective for you")
   } else if(userIncome >= zuckIncomeDaily) {
 
   } else if(userIncome >= zuckIncomeHourly) {
 
   } else {
-    
+
   }
 }
