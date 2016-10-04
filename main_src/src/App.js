ab9fa01afd26b2890009f4510e8c7f6a41647e7c
Miles Smith <miles71397@gmail.com>
2016-10-04 14:25:42 -0700

    working input method

--- main_src/src/App.js
+++ main_src/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {initialIncome: 1};
+  }
+  setIncome (event) {
+    event.preventDefault();
+    console.log(this.refs.income.value);
+    this.setState({initialIncome: this.refs.income.value});
+  }
   render() {
     return (
       <div className="App">
@@ -10,11 +18,16 @@ class App extends Component {
           <h2>Welcome to You vs Mark Zuckerberg</h2>
         </div>
         <p className="App-intro">
-          To get started, enter your income <code>src/App.js</code> and save to reload.
+          To get started, enter your income.
         </p>
+        <form onSubmit={(event) => this.setIncome(event)}>
+          <input type="number" min="1" ref="income"></input>
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
 }
+
 
 export default App;
